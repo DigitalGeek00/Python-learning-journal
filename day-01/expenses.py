90d70e1f9633bb16b1ab1f9c55659048e94473bc
Digital_Geek <digitalgeek999@gmail.com>
2026-04-26 22:19:39 +0200

    Add expenses calculation in expenses.py

---
+++ day-01/expenses.py
@@ -0,0 +1,14 @@
+alquiler = 250
+comida = 100
+gasolina = 150
+ocio = 100
+gastos_casa = 50
+gimnasio = 40
+internet = 10
+
+total = alquiler + comida + gasolina + ocio + gastos_casa + gimnasio + internet
+
+print(total)
+
+porcentaje_casa = (alquiler / total) * 100
+print(porcentaje_casa)

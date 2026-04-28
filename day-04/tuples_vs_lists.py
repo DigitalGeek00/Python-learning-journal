a37084931b6fc727c0f94f19ce86a7bbc6d2f952
Digital_Geek <digitalgeek999@gmail.com>
2026-04-28 22:03:12 +0200

    Add examples for lists and tuples in Python
    Added examples of mutable lists and immutable tuples.

---
+++ day-04/tuples_vs_lists.py
@@ -0,0 +1,12 @@
+# Lista (mutable)
+temperatura_martes = [23, 42, 54]
+
+# Tupla (inmutable)
+temperatura_miercoles = (23, 42, 54)
+
+# Ejemplo con lista
+temperatura_martes.append(60)
+print("Lista actualizada:", temperatura_martes)
+
+# Las tuplas no permiten cambios
+# temperatura_miercoles.append(60) -> ERROR

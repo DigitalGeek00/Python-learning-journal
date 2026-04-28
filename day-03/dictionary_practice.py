f5113e434ca90bbf07a8e897f99c22118b3c8e02
Digital_Geek <digitalgeek999@gmail.com>
2026-04-28 22:02:42 +0200

    Add dictionary practice script to calculate average

---
+++ day-03/dictionary_practice.py
@@ -0,0 +1,9 @@
+notas = {"María": 1.2, "Simón": 3.4, "Carlos": 2.3}
+
+# Sumar valores
+total = sum(notas.values())
+cantidad = len(notas)
+
+media = total / cantidad
+
+print("Media del diccionario:", media)
